da5fa16652be7382ec52407f5c50531c7753a91c
dryobates <jakub.stolarski@gmail.com>
2019-07-30 22:16:42 +0200

    T shows usage info when no args

--- tests/test_tcr.sh
+++ tests/test_tcr.sh
@@ -1,4 +1,21 @@
 #! /bin/sh
 
+test_shows_usage_info_when_no_args() {
+    result=`$TCR`
+
+    assertFalse $?
+    assertEquals "Usage: tcr {red,green} [msg]" "$result"
+}
+
+# stashes changes when failure and expects green
+# stashes changes when success and expects red
+# commits changes when failure and expects red
+# commits changes when success and expects green
+
+oneTimeSetUp() {
+    TCR=$(dirname $(dirname $0))/tcr
+    TCR=`realpath $TCR`
+}
+
 # Load shUnit2
 source `dirname $0`/shunit2
